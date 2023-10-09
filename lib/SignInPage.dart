2adcceff746a2b1ce46139fb7949dd3f19aefa8d
Amjadyabroudi128 <amjadyabroudi.20@gmail.com>
2023-10-10 00:36:03 +0100

    first commit

---
+++ lib/SignInPage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Signin',),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField (
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailController,
+                decoration: InputDecoration(
+
+                  border: OutlineInputBorder(),
+                  hintText: "enter your email"
+                ),
+
+              ),
+            ),
+            SizedBox(height: 15,),
+            TextField (
+                obscureText: true,
+                textAlign: TextAlign.center,
+                controller: passwordController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                  hintText: "enter your password here "
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("already sign in?"),
+                TextButton(
+                  child: Text("press me"),
+                  onPressed: (){},
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
