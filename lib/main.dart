a82adaebb6055f20b6ce1e9c3b3333f2190b6f08
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-10-14 00:11:38 +0100

    added validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:signinform/SplashScreen.dart';
 
 import 'SignInPage.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignInPage(),
+      home: SplashScreen(),
     );
   }
 }
